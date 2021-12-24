099cae596afd305ce3c7640c97f278e8d2cc6643
Oluwafemi <femikola23@gmail.com>
2021-12-24 13:29:26 +0400

    Update project

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AllSports from './allSports';
 import Sport from './sport';
-import Leagues from './leagues';
 
 const HomePage = () => (
-    <div>
-        <Routes>
-            <Route exact path="/" element={<AllSports />} />
-            <Route exact path="/sports" element={<Sport />} />
-            <Route exact path="/leagues" element={<Leagues />} />
-        </Routes>
-    </div>
-)
+  <div>
+    <Routes>
+      <Route exact path="/" element={<AllSports />} />
+      <Route exact path="/sports" element={<Sport />} />
+    </Routes>
+  </div>
+);
 
 export default HomePage;

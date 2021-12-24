1fd4674bf6286de2caca49982ebb8ae5f1ff4baf
Oluwafemi <femikola23@gmail.com>
2021-12-24 13:30:17 +0400

    Fetch Api

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,16 +1,10 @@
 const getLeagues = async () => {
-    try {
-      const response = await fetch("https://odds.p.rapidapi.com/v1/sports", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "7483d8195emshfe2c15dce789959p16abc5jsn44c1fbeb9ae5",
-            "x-rapidapi-host": "odds.p.rapidapi.com"
-        }
-    });
-      return response.json();
-    } catch (error) {
-      throw new Error(error.message);
-    }
-  };
+  try {
+    const response = await fetch('https://api.coincap.io/v2/assets');
+    return response.json();
+  } catch (error) {
+    throw new Error(error.err);
+  }
+};
 
 export default getLeagues;
